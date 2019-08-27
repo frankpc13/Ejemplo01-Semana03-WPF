5ecc483f22c4a3c3740247b75d0708a3b7d182dc
Paul Frank Pacheco Carpio <paul.frankpc@gmail.com>
2019-08-26 23:50:48 -0500

    Add project files.

---
+++ Semana03-WPF-ejemplo1/ClsDatos.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Semana03_WPF_ejemplo1
+{
+    class ClsDatos
+    {
+        public SqlConnection LeerCadena()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["kotoha"].ConnectionString);
+        }
+
+        public DataTable getListOrderDate(DateTime x, DateTime y)
+        {
+            SqlConnection connection = LeerCadena();
+            connection.Open();
+            SqlDataAdapter sqlData = new SqlDataAdapter("usp_dateToDate", connection);
+            sqlData.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlData.SelectCommand.Parameters.AddWithValue("@date1", x);
+            sqlData.SelectCommand.Parameters.AddWithValue("@date2", y);
+            DataTable dataTable = new DataTable();
+            sqlData.Fill(dataTable);
+            connection.Close();
+            return dataTable;
+        }
+
+        public DataTable getDetailsList(int x)
+        {
+            SqlConnection connection = LeerCadena();
+            connection.Open();
+            SqlDataAdapter sqlData = new SqlDataAdapter("usp_detailOrder", connection);
+            sqlData.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlData.SelectCommand.Parameters.AddWithValue("@orderId", x);
+            DataTable dataTable = new DataTable();
+            sqlData.Fill(dataTable);
+            connection.Close();
+            return dataTable;
+        }
+
+        public double totalOrder(Int32 idOrder)
+        {
+            SqlConnection connection = LeerCadena();
+            connection.Open();
+            SqlDataAdapter sqlData = new SqlDataAdapter("usp_totalAmountOrder2", connection);
+            sqlData.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlData.SelectCommand.Parameters.AddWithValue("@orderId", idOrder);
+            sqlData.SelectCommand.Parameters
+                .Add("@Total", SqlDbType.Money).Direction = ParameterDirection.Output;
+            sqlData.SelectCommand.ExecuteScalar();
+            Int32 total = Convert.ToInt32(sqlData.SelectCommand.Parameters["@total"].Value);
+            connection.Close();
+            return total;
+        }
+
+
+    }
+}
---
+++ Semana03-WPF-ejemplo1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Semana03_WPF_ejemplo1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClsDatos obj = new ClsDatos();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConsultar_Click(object sender, RoutedEventArgs e)
+        {
+            dataGridPedido.ItemsSource = obj
+                .getListOrderDate(Convert.ToDateTime(txtFechaInicio.Text), Convert.ToDateTime(txtFechaFin.Text))
+                .DefaultView;
+
+
+        }
+
+        private void DataGridPedido_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int idPedido;
+            var item = dataGridPedido.SelectedItem as DataRowView;
+            if (item == null) return;
+            idPedido = Convert.ToInt32(item.Row["IdPedido"]);
+            dataGridDetallePedido.ItemsSource = obj.getDetailsList(idPedido).DefaultView;
+            txtTotal.Text = Convert.ToString(obj.totalOrder(idPedido));
+        }
+    }
+}
